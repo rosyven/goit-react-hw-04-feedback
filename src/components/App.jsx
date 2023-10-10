bf9ab2284e1d119756e16e22f5f9e78e57f489ae
rosyven <93422035+rosyven@users.noreply.github.com>
2023-10-10 20:48:22 +0300

    state change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,28 +6,30 @@ import { Statistics } from "./Feedback/Statistics";
 import { useState } from "react";
 
 export const App = () => {
-  const [state, setState] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  });
-  // const [good, setGood] = useState(0);
-  // const [neutral, setNeutral] = useState(0);
-  // const [bad, setBad] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
    const handleFeedback = (type) => {
-     setState((prevState) => ({
-       ...prevState,
-       [type]: prevState[type] + 1
-     }));
+    switch (type) {
+      case "good":
+        setGood((prevGood) => prevGood + 1);
+        break;
+      case "neutral":
+        setNeutral((prevNeutral) => prevNeutral + 1);
+        break;
+      case "bad":
+        setBad((prevBad) => prevBad + 1);
+        break;
+      default:
+        break;
+    }
   };
 
   const countTotalFeedback = () => {
-    const { good, neutral, bad } = state;
     return good + neutral + bad;
   };
 
-  const { good, neutral, bad } = state;
   const total = countTotalFeedback();
   const positivePercentage = total === 0 ? 0 : Math.round((good / total) * 100);
   
@@ -35,7 +37,7 @@ export const App = () => {
     <div>
       <Section>
         <FeedbackOptions
-          options={Object.keys(state)}
+          options={["good", "neutral", "bad"]}
           onClick={handleFeedback}
         />
 
